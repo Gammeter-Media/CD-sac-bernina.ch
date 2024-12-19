ffb57b9019b2c51c8ffa890a86409939b167a5cf
Raphael Bauer <r.bauer@gammetermedia.ch>
2024-12-19 17:11:04 +0100

    url missmatch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,10 +9,10 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://gammeter-media.github.io/CD-sac-bernina.ch/",
+    url: "https://gammeter-media.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/CD-sac-bernina.ch/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
