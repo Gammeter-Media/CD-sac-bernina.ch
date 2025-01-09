e05987970c1d335eb2f5e22a6d1cf917df933ccf
j.bisaz <j.bisaz@gammetermedia.ch>
2025-01-09 14:07:48 +0100

    add stylings

--- src/components/colorDisplay/index.js
+++ src/components/colorDisplay/index.js
@@ -3,19 +3,20 @@ import styles from '@site/src/components/HomepageFeatures/styles.module.css';
 
 
 // Primary component
-const ColorBoxes = ({ web, RGB,  pantone, cmyk}) => {
-return (
-<div className={styles.logofarben}>
-    <span>pantone: {pantone}</span>
-    <span>cmyk: {cmyk}</span>
-    <span>web: {web}</span>
-    <span>rgb: {RGB}</span>
-    
-    <div style={{ backgroundColor: web, width: "100%", height: "100%" }}>
-    </div>
-</div>
+const ColorBoxes = ({ web, RGB, pantone, cmyk }) => {
+    return (
+        <div className={styles.logofarben}>
+            <span>pantone: {pantone}</span>
+            <span>cmyk: {cmyk}</span>
+            <span>web: {web}</span>
+            <span>rgb: {RGB}</span>
 
-)};
+            <div style={{ backgroundColor: web, width: "100%", height: "100%" }}>
+            </div>
+        </div>
+
+    )
+};
 
 
 
@@ -26,7 +27,7 @@ const AddClass = ({ className }) => {
     );
 };
 
-const Schmuckfarben = ({ Primary, primarycmyk, secondary, secondarycmyk}) => {
+const Schmuckfarben = ({ Primary, primarycmyk, secondary, secondarycmyk }) => {
     return (
         <div className={styles.schmuckfarben} style={{ backgroundColor: Primary }}>
             <div className={styles.schmukfarbenTop} style={{ backgroundColor: secondary }}>
