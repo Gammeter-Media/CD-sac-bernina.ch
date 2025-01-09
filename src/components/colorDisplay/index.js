1b6ecaca6ef97870f8aac91687496b9cae0cb112
j.bisaz <j.bisaz@gammetermedia.ch>
2025-01-09 08:58:51 +0100

    add stylings WIP

--- src/components/colorDisplay/index.js
+++ src/components/colorDisplay/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-const Primary = ({ color, className, children }) => {
-  return (
-    <div style={{ backgroundColor: color }} className={className}>
-      {children}
+// Primary component
+const ColorBoxes = ({ hex, className, pantone}) => {
+    return (
+    <div className={className}>
+        <span>{pantone}</span>
+        <span>{hex}</span>       
+        
+        <div style={{ backgroundColor: hex, width: "100%", height: "100%" }}>
+        </div>
     </div>
-  );
+
+    )};
+
+// ColorContainer component
+const addClass = ({ className, children }) => {
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    );
 };
 
-export default Primary;
+
+
+
+// Export components
+export { ColorBoxes, addClass }; // Named exports
+
+// Optionally, you can set one as the default export if needed
+export default ColorBoxes;
+
+
