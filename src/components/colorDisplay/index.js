2f2765982a2058a35f05ae56bd2275a0a0f27165
j.bisaz <j.bisaz@gammetermedia.ch>
2025-01-09 13:54:39 +0100

    create schmuckfarben elements

--- src/components/colorDisplay/index.js
+++ src/components/colorDisplay/index.js
@@ -3,50 +3,47 @@ import styles from '@site/src/components/HomepageFeatures/styles.module.css';
 
 
 // Primary component
-const ColorBoxes = ({ web, className, RGB,  pantone, cmyk}) => {
-    return (
-    <div className={styles.Logofarben}>
-        <span>pantone: {pantone}</span>
-        <span>cmyk: {cmyk}</span>       
-        <span>web: {web}</span>       
-        <span>rgb: {RGB}</span>       
-        
-        <div style={{ backgroundColor: web, width: "100%", height: "100%" }}>
-        </div>
-    </div>
-
-    )};
-
-
-
-
+const ColorBoxes = ({ web, RGB,  pantone, cmyk}) => {
+return (
+<div className={styles.logofarben}>
+    <span>pantone: {pantone}</span>
+    <span>cmyk: {cmyk}</span>
+    <span>web: {web}</span>
+    <span>rgb: {RGB}</span>
     
+    <div style={{ backgroundColor: web, width: "100%", height: "100%" }}>
+    </div>
+</div>
+
+)};
+
+
 
 // ColorContainer component
-const addClass = ({ className}) => {
+const AddClass = ({ className }) => {
     return (
-        <div className={className}>
-        </div>
+        <div className={className}></div>
     );
 };
 
-// Schmuckfarben
-const Schmuckfarben = ({ className, color}) => {
+const Schmuckfarben = ({ Primary, primarycmyk, secondary, secondarycmyk}) => {
     return (
-        <div className={styles.Schmuckfarben} style={{backgroundColor: color}}>
-            <div className={styles.schmukfarbenTop}>
+        <div className={styles.schmuckfarben} style={{ backgroundColor: Primary }}>
+            <div className={styles.schmukfarbenTop} style={{ backgroundColor: secondary }}>
+                <div>
+                    <span>CMYK: {secondarycmyk}</span>
+                    <span>RGB: {secondary}</span>
+                </div>
+            </div>
+            <div>
+                <span>CMYK: {primarycmyk}</span>
+                <span>RGB: {Primary}</span>
             </div>
         </div>
     );
 };
 
-
-
-
-// Export components
-export { ColorBoxes, addClass, Schmuckfarben }; // Named exports
-
-// Optionally, you can set one as the default export if needed
-export default ColorBoxes;
+export { ColorBoxes, AddClass, Schmuckfarben };
+export default Schmuckfarben;
 
 
