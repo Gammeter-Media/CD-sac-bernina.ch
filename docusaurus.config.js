b16732aaa1584ced3b3e1af98d477e97af523100
Raphael Bauer <r.bauer@gammetermedia.ch>
2025-01-15 11:59:43 +0100

    main title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
             position: "left",
-            label: "Corporate Design",
+            label: "Corporate Design Manual",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           // {
