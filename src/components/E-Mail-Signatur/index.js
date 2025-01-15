e7193b1864d98fa905c17c4ba73af5d7cc1f8548
j.bisaz <j.bisaz@gammetermedia.ch>
2025-01-15 09:48:43 +0100

    add Signatur functionalitiy

--- src/components/E-Mail-Signatur/index.js
+++ src/components/E-Mail-Signatur/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@site/src/css/E-Mail-Signatur.css';
 
+const Signatur = ({ Vorname = '', Name = '', Funktion = '', EMail = '' }) => {
+    // State to manage form inputs
+    const [formValues, setFormValues] = useState({
+        vorname: Vorname,
+        name: Name,
+        funktion: Funktion,
+        email: EMail,
+    });
 
-// Primary component
-const Signatur = ({}) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+
+    let test = `<p><b>` + formValues.vorname +` ` + formValues.name +`/b>
+`+ formValues.funktion + `</p>
+<p><b>Schweizer Alpen-Club SAC | Sektion Bernina</b>
+<a href="` + formValues.email +`">` + formValues.email  +`</a> | <a href="https://www.sac-bernina.ch">sac-bernina.ch</a></p>`
+
     return (
-        <div className="signatur">        
-                <input type="text"></input>
-                <input type="text"></input>
-                <input type="text"></input>
-                <input type="email" name="" id=""></input>
+        <div className="signatur">
+            <input type="text" id="hello" name="vorname" placeholder="Vorname" value={formValues.vorname} onChange={handleChange}/>
+            <input type="text" name="name" placeholder="Name" value={formValues.name} onChange={handleChange}/>
+            <input type="text" name="funktion" placeholder="Funktion" value={formValues.funktion} onChange={handleChange}/>
+            <input type="email" name="email" placeholder="E-Mail"value={formValues.email} onChange={handleChange}/>     
+
+            <pre>
+                <code>
+                    {test}
+                    
+                </code>
+            </pre>
         </div>
-    )
+        
+
+        
+    );
+
+
 };
+
+
+export { Signatur};
+
+export default Signatur;
+
