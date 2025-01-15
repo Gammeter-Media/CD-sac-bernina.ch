d2250495d793f2a97ffed0c82cb274929f5fa125
j.bisaz <j.bisaz@gammetermedia.ch>
2025-01-15 08:17:08 +0100

    create React element: Signatur

--- src/components/E-Mail-Signatur/index.js
+++ src/components/E-Mail-Signatur/index.js
@@ -2,3 +2,14 @@ import React from 'react';
 import '@site/src/css/E-Mail-Signatur.css';
 
 
+// Primary component
+const Signatur = ({}) => {
+    return (
+        <div className="signatur">        
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="email" name="" id=""></input>
+        </div>
+    )
+};
