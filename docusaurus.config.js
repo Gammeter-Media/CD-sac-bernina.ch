11ec94ac7b6bb6c5708c503fb995f30aee52173a
Raphael Bauer <r.bauer@gammetermedia.ch>
2025-02-10 16:43:49 +0100

    update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
     url: "https://gammeter-media.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/CD-sac-bernina.ch/",
+    baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
